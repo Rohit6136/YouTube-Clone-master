882e828804c77592d543415e6fb8b67daebce780
Rohit6136 <rohitkumar2017222@gmail.com>
2023-04-20 15:54:20 +0530

    frontend project

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+    { name: 'New', icon: <HomeIcon />, },
+    { name: 'NIT KKR', icon: <CodeIcon />, },
+    { name: 'Coding', icon: <CodeIcon />, },
+    { name: 'ReactJS', icon: <CodeIcon />, },
+    { name: 'NextJS', icon: <CodeIcon />, },
+    { name: 'Music', icon: <MusicNoteIcon /> },
+    { name: 'Education', icon: <SchoolIcon />, },
+    { name: 'Podcast', icon: <GraphicEqIcon />, },
+    { name: 'Movie', icon: <OndemandVideoIcon />, },
+    { name: 'Gaming', icon: <SportsEsportsIcon />, },
+    { name: 'Live', icon: <LiveTvIcon />, },
+    { name: 'Sport', icon: <FitnessCenterIcon />, },
+    { name: 'Fashion', icon: <CheckroomIcon />, },
+    { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+    { name: 'Comedy', icon: <TheaterComedyIcon />, },
+    { name: 'Gym', icon: <FitnessCenterIcon />, },
+    { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = 'https://www.youtube.com/channel/UCeNBt-YR8d6xpfGtna8-RSg';
+export const demoVideoUrl = 'https://www.youtube.com/watch?v=M8v9UDaEN7k';
+export const demoChannelTitle = 'NIT Kurukshetra';
+export const demoVideoTitle = 'IDY-2018 Part-I';
+export const demoProfilePicture = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FNITKkrBGT%2F&psig=AOvVaw0K1j-ToYhs0uRuZYb80N8G&ust=1664029319814000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNC6_LeOq_oCFQAAAAAdAAAAABAD'
